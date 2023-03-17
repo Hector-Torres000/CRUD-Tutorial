c5ffd79c52abcc721b7254b70cbd26f817454da0
Hector Torres <h.s.torres96@gmail.com>
2023-03-17 00:24:26 -0700

    mongoDB with altas functionality good

--- server.js
+++ server.js
@@ -1,11 +1,39 @@
-console.log('whatever');
+const bodyParser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
 const express = require('express');
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: true }));
+
+MongoClient.connect(
+  'mongodb+srv://hstorres96:Ely6Z46d2W02BXWm@cluster0.ae7mzee.mongodb.net/test?retryWrites=true&w=majority'
+)
+  .then((client) => {
+    console.log('connected to database');
+    const db = client.db('favorite-quotes');
+    const quotesCollection = db.collection('quotes');
+
+    app.get('/', (req, res) => {
+      db.collection('quotes')
+        .find()
+        .toArray()
+        .then((results) => console.log(results))
+        .catch((err) => console.log(err));
+
+      res.sendFile(__dirname + '/index.html');
+    });
+
+    app.post('/quotes', (req, res) => {
+      quotesCollection
+        .insertOne(req.body)
+        .then((result) => {
+          res.redirect('/');
+        })
+        .catch((err) => console.log(err));
+    });
+  })
+  .catch((err) => console.log(err));
+
 app.listen(3000, () => {
   console.log('listening on 3000');
-});
-
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
 });
